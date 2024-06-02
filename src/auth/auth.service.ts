9237547132f36b63cb47d209f8a3e2f8f683da19
Mansi Rathi <Mansi.Rathi0@experian.com>
2024-06-01 19:10:19 -0500

    feat: Adds singin and signin and signup methods (Initials)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable({})
 export class AuthService {
-  login() {}
-  signup() {}
+  signup() {
+    return { msg: 'I am Signed Up!' };
+  }
+  signin() {
+    return { msg: 'I am Signed In!' };
+  }
 }
