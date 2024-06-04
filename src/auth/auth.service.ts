c8aee1a0c8b95fd90e81df6172c52d1de3a7f1d1
Mansi Rathi <Mansi.Rathi0@experian.com>
2024-06-04 18:00:59 -0500

    feat: handles unique user error

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,22 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import * as argon from 'argon2';
 import { AuthDto } from './dto';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 @Injectable({})
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
   async signup(dto: AuthDto) {
     const hash = await argon.hash(dto.password);
     //save the new user in the DB
-    const user = await this.prismaService.user.create({
-      data: {
-        email: dto.email,
-        hash,
-      },
-    });
-    delete user.hash;
-    //return the new user
-    return user;
+    try {
+      const user = await this.prismaService.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+      delete user.hash;
+      //return the new user
+      return user;
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials Taken!');
+        }
+      }
+      throw error;
+    }
   }
   signin() {
     return { msg: 'I am Signed In!' };
