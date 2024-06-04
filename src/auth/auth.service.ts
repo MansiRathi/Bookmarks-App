2862099b3d214f701a82e2dab20a9777a0f4b07d
Mansi Rathi <Mansi.Rathi0@experian.com>
2024-06-04 17:43:55 -0500

    feat: Updates Models and adds argon to hash the user pwd

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-
+import * as argon from 'argon2';
+import { AuthDto } from './dto';
 @Injectable({})
 export class AuthService {
   constructor(private prismaService: PrismaService) {}
-  signup() {
-    return { msg: 'I am Signed Up!' };
+  async signup(dto: AuthDto) {
+    const hash = await argon.hash(dto.password);
+    //save the new user in the DB
+    const user = await this.prismaService.user.create({
+      data: {
+        email: dto.email,
+        hash,
+      },
+    });
+    delete user.hash;
+    //return the new user
+    return user;
   }
   signin() {
     return { msg: 'I am Signed In!' };
